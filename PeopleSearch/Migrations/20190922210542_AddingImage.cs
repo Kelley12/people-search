2958772897b3a887c35af522765c14065b2bbead
Blake Kelley <blakekelley127@gmail.com>
2019-09-22 18:34:39 -0600

    Adding seed images

--- PeopleSearch/Migrations/20190922210542_AddingImage.cs
+++ PeopleSearch/Migrations/20190922210542_AddingImage.cs
@@ -11,6 +11,69 @@ namespace PeopleSearch.Migrations
                 table: "People",
                 type: "varchar(256)",
                 nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 1,
+                columns: new[] { "ImagePath", "Interests" },
+                values: new object[] { "Resources\\Images\\female1.jpeg", "fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum eu" });
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 3,
+                column: "ImagePath",
+                value: "Resources\\Images\\female2.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 5,
+                column: "ImagePath",
+                value: "Resources\\Images\\female3.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 6,
+                column: "ImagePath",
+                value: "Resources\\Images\\female4.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 8,
+                column: "ImagePath",
+                value: "Resources\\Images\\male1.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 10,
+                column: "ImagePath",
+                value: "Resources\\Images\\male2.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 11,
+                column: "ImagePath",
+                value: "Resources\\Images\\male3.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 12,
+                column: "ImagePath",
+                value: "Resources\\Images\\male4.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 13,
+                column: "ImagePath",
+                value: "Resources\\Images\\male5.jpeg");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -18,6 +81,69 @@ namespace PeopleSearch.Migrations
             migrationBuilder.DropColumn(
                 name: "ImagePath",
                 table: "People");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 1,
+                columns: new[] { "ImagePath", "Interests" },
+                values: new object[] { null, "fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum eu," });
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 3,
+                column: "ImagePath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 5,
+                column: "ImagePath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 6,
+                column: "ImagePath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 8,
+                column: "ImagePath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 10,
+                column: "ImagePath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 11,
+                column: "ImagePath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 12,
+                column: "ImagePath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "PersonId",
+                keyValue: 13,
+                column: "ImagePath",
+                value: null);
         }
     }
 }
